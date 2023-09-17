e7e28d8589e7d84d66f68bfc3ccf94a358fb4e68
Lucas M. Ansieta M <lansieta@MacBook-Air-LAM.local>
2023-09-16 22:27:02 -0300

    v1.2

---
+++ proyecto_final/myapp/new_api.py
@@ -0,0 +1,10 @@
+from urllib.parse import quote
+
+def url_to_qr(text):
+    # Codificar el texto en URL
+    encoded_text = quote(text)
+    
+    # Crear la URL con el texto codificado
+    url = f"https://api.qrserver.com/v1/create-qr-code/?data={encoded_text}"
+    
+    return url
