7082397252a07ae80953375d69e6519982aa4d95
Gabo <gdelgado@usm.cl>
2023-10-13 19:16:07 -0300

    0

--- proyecto_final/myapp/urls.py
+++ proyecto_final/myapp/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about/', views.about, name="about"),  #No priotidad 1
+    path('about/', views.about, name="about"),
     path('users/', views.user, name="users"),
     path('anadir/', views.registro_usuario, name="anadir"),
-    path('blog/', views.blog, name="blog"),  #no prioridad 2
+    path('blog/', views.blog, name="blog"), 
     path('perfil/', views.perfil, name="perfil"),
     path('signup/', views.signup, name="signup"),
     path('signout/', views.signout, name="signout"),
