809ab0cb7c1cdfbf9df77e706c2dd01ad2f42db4
Gabo <gdelgado@usm.cl>
2023-10-14 00:05:22 -0300

    configuracion setings host

--- proyecto_final/qr4health/settings.py
+++ proyecto_final/qr4health/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,8 @@ SECRET_KEY = 'django-insecure-#%z427q0n*b_p3)b#d*xlsxvf%k@a96v!e&3urfau-k^y#eozg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['0.0.0.0']
+PORT = int(os.environ.get("PORT", 8000))
 
 # Application definition
 
